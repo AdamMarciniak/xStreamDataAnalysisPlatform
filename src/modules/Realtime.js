936b665a80be0b0adbc729da3a29b8a99bc596e4
Adam Marciniak <adam668@gmail.com>
2019-10-29 21:08:03 -0700

    scrapped fake data for now. ESLinted everything.

--- src/modules/Realtime.js
+++ src/modules/Realtime.js
@@ -1,37 +1,40 @@
 
-import * as DataManager from './DataManager.js';
+import * as DataManager from './DataManager';
 
-export const startRealtimeGathering = () => {
-  DataManager.setupRealtimeData();
-  establishWebsockets();
-}
+let realtimeFlag = 0;
+
+export const getRealtimeFlag = () => realtimeFlag;
 
 let timeIndex = 0;
 const establishWebsockets = () => {
-
   const ws = new WebSocket('ws://192.99.54.136:1024');
 
-  ws.onopen = function (event) {
+  ws.onopen = function onOpen(event) {
     ws.send('Web Client Connected');
   };
 
-  ws.onmessage = function (event) {
+  ws.onclose = function onClose(event) {
+    realtimeFlag = 0;
+  };
+
+  ws.onmessage = function onMessage(event) {
+    realtimeFlag = 1;
     const message = event.data;
-    if (message[0] == 'x') {
-      const xAngle = message.substring(1);
+    if (message[0] === 'x') {
+      const xAngle = parseFloat(message.substring(1));
       DataManager.addToRealtimeData('Body Sway', timeIndex, xAngle);
       DataManager.addToRealtimeData('Body Sway2', timeIndex, 2 * xAngle);
       DataManager.addToRealtimeData('Body Sway3', timeIndex, 3 * xAngle);
-      DataManager.addToRealtimeData('Body Sway4', timeIndex, 4* Math.sin(xAngle));
+      DataManager.addToRealtimeData('Body Sway4', timeIndex, 4 * Math.sin(xAngle));
 
       timeIndex += 0.1;
-
     }
-    if (message[0] == 'y') {
-      
-      const yAngle = message.substring(1);
-    }
-
   };
+};
 
-}
+const startRealtimeGathering = () => {
+  DataManager.setupRealtimeData();
+  establishWebsockets();
+};
+
+export default startRealtimeGathering;
